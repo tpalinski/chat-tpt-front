538537ac3b6d3edc509af807aff63f43a07ae641
tpalinski <tpalinski@icloud.com>
2023-01-26 17:29:07 +0100

    Add auto scroll

--- client/src/components/MessageBox.tsx
+++ client/src/components/MessageBox.tsx
@@ -5,28 +5,54 @@ import { WebRepository } from '../api/api';
 type Props = {}
 type Messages = string[]
 type MessagesState = {
-    messages: Messages
+    messages: Messages,
+    message: string
 }
 
 export class MessageBox extends React.Component {
 
     state: MessagesState = {
-        messages: []
+        messages: [],
+        message: ""
     }
 
     constructor(props: Props) {
         super(props);
-        this.state = {messages: ["Welcome to the new channel"]};
+        this.state = {messages: ["Welcome to the new channel"], message: ""};
       }
 
     public UpdateMessages(newMessage: string) {
         this.setState((prevState: MessagesState) => ({
-            messages: [...prevState.messages, newMessage]
+            messages: [...prevState.messages, newMessage],
+            message: prevState.message
         }))
-    }    
+    }
+    
+    handleMessageChange(newMessage: string){
+        this.setState((prevState: MessagesState) => ({
+            message: newMessage,
+            messages: prevState.messages
+        }));
+    }
+
+
+    handleSubmit(e: React.FormEvent<HTMLFormElement>){
+        e.preventDefault();
+        this.setState((prevState: MessagesState) => ({
+            message: "",
+            messages: prevState.messages
+        }))
+        WebRepository.sendMessage(this.state.message)
+    }
+
 
     componentDidMount(): void {
         WebRepository.setMessagesContainer(this);
+    }
+
+    componentDidUpdate(): void {
+        let scrollbox = document.getElementsByClassName("MessageBox")[0]
+        scrollbox.scrollTop = scrollbox.scrollHeight
     }
 
     render() { return (
@@ -39,7 +65,10 @@ export class MessageBox extends React.Component {
                     </>
                 ))}
             </div>
-            <button onClick={() => WebRepository.sendMessage("debug")}> Debug add message</button>
+            <form onSubmit={(e) => this.handleSubmit(e)}>
+                <input onChange={(e) => this.handleMessageChange(e.target.value)} value={this.state.message} type="text"/>
+                <button> Send Message </button>
+            </form>
         </>  
         )
     }
